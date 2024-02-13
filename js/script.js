42d2063d6137f747b1346bdc2139a00e5c4f8966
Steffano da Cruz <steffanodacruz@outlook.com>
2024-02-13 13:26:15 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const subscriptionInp = document.querySelector(".subscription-input");
+const subscriptionBtn = document.querySelector(".subscription-button");
+
+subscriptionBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const inputValue = subscriptionInp.value;
+  console.log(inputValue);
+});
