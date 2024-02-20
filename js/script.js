4f2bd8f36064cdb91860225fb11db3601c788c0c
Steffano da Cruz <steffanodacruz@outlook.com>
2024-02-20 11:27:39 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,8 +2,21 @@
 
 const subscriptionInp = document.querySelector(".subscription-input");
 const subscriptionBtn = document.querySelector(".subscription-button");
+const subscriptionCont = document.querySelector(".subscription-container");
+const successCont = document.querySelector(".success-container");
+const subscriptionError = document.querySelector(".subscription-error");
 
 subscriptionBtn.addEventListener("click", function () {
   const inputValue = subscriptionInp.value;
-  console.log(inputValue);
+
+  if (inputValue.includes("@")) {
+    subscriptionCont.classList.toggle("hidden");
+    successCont.classList.toggle("hidden");
+  } else {
+    subscriptionError.classList.toggle("hidden");
+    subscriptionError.style.color = "#ff6257";
+    subscriptionInp.style.color = "#ff6257";
+    subscriptionInp.style.borderColor = "#ff6257";
+    subscriptionInp.style.backgroundColor = "#ffa8a34d";
+  }
 });
